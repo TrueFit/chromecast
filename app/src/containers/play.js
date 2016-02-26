27d4faf60a60411e1c4627cf3be58df0b52f00f8
jgretz <jgretz@truefitsolutions.com>
2016-02-25 23:33:42 -0500

    add receiver back in to see if that lets it not quit

--- app/src/containers/play.js
+++ app/src/containers/play.js
@@ -1,10 +1,14 @@
 
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import CastAway from '../../vendor/castaway/cast-away';
 
 class Play extends Component {
   constructor(props) {
     super(props);
+
+    const castAway = new window.CastAway();
+    this.receiver = castAway.receive();
 
     this.state = {
       src: "https://unsplash.it/1920/1080"
