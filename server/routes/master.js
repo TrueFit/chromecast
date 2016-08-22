46f15cf41fb288a2e9c4937eee7d73860bbb3a76
jgretz <jgretz@truefitsolutions.com>
2016-08-22 15:38:03 -0400

    factor out mdns to dev tools

--- server/routes/master.js
+++ server/routes/master.js
@@ -2,26 +2,32 @@
 var _ = require('lodash');
 var Client = require('castv2-client').Client;
 var Web = require('castv2-web').Web;
-var mdns = require('mdns');
 
-// use a headless browser to find the casts
 var casts = [];
-var browser = mdns.createBrowser(mdns.tcp('googlecast'));
-browser.on('serviceUp', (service) => {
-  console.log('found device "%s" at %s:%d', service.name, service.addresses[0], service.port);
-
-  casts.push({
-    name: service.name,
-    address: service.addresses[0],
-    port: service.port
-  });
-
-  browser.stop();
-});
-browser.start();
 
 // listen to endpoint to connect and launch
 module.exports = (router) => {
+  router.get('/master-load', (req, res) => {
+    // use a headless browser to find the casts
+    var mdns = require('mdns');
+
+    var browser = mdns.createBrowser(mdns.tcp('googlecast'));
+    browser.on('serviceUp', (service) => {
+      console.log('found device "%s" at %s:%d', service.name, service.addresses[0], service.port);
+
+      casts.push({
+        name: service.name,
+        address: service.addresses[0],
+        port: service.port
+      });
+
+      browser.stop();
+    });
+    browser.start();
+
+    res.json({ result: 'Casts Loaded' });
+  });
+
 	router.get('/master', (req, res) => {
     var castName = req.query.c;
     if (!castName) {
